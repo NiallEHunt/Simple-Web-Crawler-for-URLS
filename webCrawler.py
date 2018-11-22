d758efaa5983346676384c60680318a2832c8f69
NiallEHunt <NiallEHunt@gmail.com>
2018-11-22 20:36:36 +0000

    Deals with starting URLs that won't provide N unique urls

--- webCrawler.py
+++ webCrawler.py
@@ -29,6 +29,7 @@ def find_links_on_page(url, N):
 # Using a set as each element in a set can only appear once. Therefore, we end
 # up with a set of all the unique links
 unique_links = set()
+visited_links = set()
 
 parser = argparse.ArgumentParser(description="Scrape N unique URLs from an inputted URL. The default values are N = 100 and url = 'https://www.bbc.com'")
 
@@ -38,15 +39,30 @@ parser.add_argument('-url', help="The starting URL to scrape from", default='htt
 args = parser.parse_args()
 
 url = args.url
+unique_links.add(url)
 upper_bound = args.n
 finished = False
+
 # Will find links from the given url. If the upper bound is not met it will use a random
 # url from the set to continue scraping from
 while not finished:
     finished = find_links_on_page(url, upper_bound)
-    url = unique_links.pop()
-    unique_links.add(url)
+
+    visited_links.add(url)
+
+    # If we have visited all the unique links we have found we break as we can't crawl through any more sites
+    if visited_links == unique_links:
+        break
+
+    # The new url to scrape has to be one that we haven't visited before
+    while url in visited_links:
+        url = unique_links.pop()
+        unique_links.add(url)
 
 # Print each url on a new line
 for link in unique_links:
     print(link)
+
+# If we didn't find N links print the number we found
+if len(unique_links) < upper_bound:
+    print("Only found " + str(len(unique_links)) + " unique links")
