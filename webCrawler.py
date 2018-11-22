c80c049860a144635a2911804afe1329d25b627a
NiallEHunt <NiallEHunt@gmail.com>
2018-11-22 20:01:50 +0000

    Takes in N and start URL from arguments

--- webCrawler.py
+++ webCrawler.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import argparse
 
 # Finds all the links on the given page and adds them to the unique_links set
 # returns True if the unique_links set is full (ie. has N links)
@@ -29,13 +30,23 @@ def find_links_on_page(url, N):
 # up with a set of all the unique links
 unique_links = set()
 
-# Set the upper bound of the set (ie. how many unique urls you want to print)
-upper_bound = 150
+parser = argparse.ArgumentParser(description="Scrape N unique URLs from an inputted URL. The default values are N = 100 and url = 'https://www.bbc.com'")
+
+parser.add_argument('-n', type=int, help="The number of unique URLs you want to find", default=100)
+parser.add_argument('-url', help="The starting URL to scrape from", default='https://www.bbc.com')
+
+args = parser.parse_args()
+
+url = args.url
+upper_bound = args.n
 finished = False
-url = 'https://www.youtube.com/watch?v=oYqyzctGCHs'
+# Will find links from the given url. If the upper bound is not met it will use a random
+# url from the set to continue scraping from
 while not finished:
     finished = find_links_on_page(url, upper_bound)
     url = unique_links.pop()
     unique_links.add(url)
+
+# Print each url on a new line
 for link in unique_links:
     print(link)
