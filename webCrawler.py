cd6cc07327eca9b236e28a4f33aa7e26caa63e49
NiallEHunt <NiallEHunt@gmail.com>
2018-11-22 15:16:27 +0000

    Links added to a set. Set contains only unique urls

--- webCrawler.py
+++ webCrawler.py
@@ -1,11 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+unique_links = set()
+
 def find_links_on_page(url):
     raw_data = requests.get(url).text
     scraper = BeautifulSoup(raw_data, "html.parser")
     for tag in scraper.findAll('a'):
         link = tag.get('href')
-        print(link)
+        if link[0] == '/':
+            link = url + link
+        unique_links.add(link)
 
 find_links_on_page('http://www.bbc.com')
+print(len(unique_links))
